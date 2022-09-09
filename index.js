a442da3a5dfb34146abde1dd7c1b32a70339549d
Charles <charlie.david@me.com>
2022-09-09 15:05:50 -0600

    commit tree 52f786d089aced29e3e071def7e886c8f834f71d

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(a, b){return a + b}
+
+function subtract(a, b){return a - b}
+
+function multiply(a, b){return a * b}
+
+function divide(a, b){return a / b}
+
+let number = 10
+
+function  increment(number){return number += 1}
+
+function decrement(number){return number -= 1}
+
+let string = "2.14"
+function makeInt(string){return parseInt(string, 10)}
+
+function preserveDecimal(string){return parseFloat(string)}
